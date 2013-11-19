59cf2a026b29a1f4b789aa2b6621e729d3e13a87
ubx <andreas.luethi@gmx.net>
2013-11-19 18:45:31 +0100

    Send bugsense data onl over WiFi.

--- src/ch/luethi/skylinestracker/MainActivity.java
+++ src/ch/luethi/skylinestracker/MainActivity.java
@@ -53,6 +53,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        BugSenseHandler.sendDataOverWiFiOnly();
         BugSenseHandler.initAndStartSession(this, "a9b9af2d");
         positionService = new Intent(this, PositionService.class);
         if (getIntent().hasExtra(IPADDRESS)) {
