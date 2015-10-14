359851926b48db81b663fc516087150ee340d773
andreas <andreas.luethi@gmx.net>
2015-10-14 21:34:16 +0200

    Prepare for integration testing #3.

--- Tests/start-emulator.sh
+++ Tests/start-emulator.sh
@@ -4,7 +4,7 @@
 adb -s emulator-5554 install -r /home/andreas/IdeaProjects/SkyLinesTracker/SkyLinesTracker.apk
 adb push /home/andreas/IdeaProjects/SkyLinesTracker/Tests/ch.luethi.skylinestracker_preferences.xml data/data/ch.luethi.skylinestracker/shared_prefs/
 adb -s emulator-5554 shell ps
-export ip=$(hostname -I | awk '{print $1}')
 adb shell input keyevent 82
+export ip=$(hostname -I | awk '{print $1}')
 adb -s emulator-5554 shell am start -n ch.luethi.skylinestracker/ch.luethi.skylinestracker.MainActivity -a android.intent.action.MAIN -c android.intent.category.LAUNCHER -e ISTESTING true -e TESTING_IP ${ip}
 python /home/andreas/IdeaProjects/SkyLinesTracker/Tests/integration.py
