07bdbe617b719b761a78ed608f5f803c821e63d5
andreas <andreas.luethi@gmx.net>
2015-10-19 08:30:22 +0200

    Prepare for integration testing #5.

--- Tests/start-emulator.sh
+++ Tests/start-emulator.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #
-/home/andreas/opt/android-sdk-linux/tools/emulator -avd Device -netspeed full -netdelay none -no-boot-anim &
-adb -s emulator-5554 install -r /home/andreas/IdeaProjects/SkyLinesTracker/SkyLinesTracker.apk
-adb push /home/andreas/IdeaProjects/SkyLinesTracker/Tests/ch.luethi.skylinestracker_preferences.xml data/data/ch.luethi.skylinestracker/shared_prefs/
+EMULATOR_DIR="/home/andreas/opt/android-sdk-linux/tools"
+PROJECT_DIR="/home/andreas/IdeaProjects/SkyLinesTracker"
+TEST_DIR="/home/andreas/IdeaProjects/SkyLinesTracker/Tests"
+#
+$EMULATOR_DIR/emulator -avd Device -netspeed full -netdelay none -no-boot-anim &
+adb -s emulator-5554 install -r $PROJECT_DIR/SkyLinesTracker.apk
+adb push $TEST_DIR/ch.luethi.skylinestracker_preferences.xml data/data/ch.luethi.skylinestracker/shared_prefs/
 adb -s emulator-5554 shell ps
 
 ###nc -l -u 5597 | xxd -b &
@@ -10,5 +14,5 @@ adb -s emulator-5554 shell ps
 adb shell input keyevent 82
 export ip=$(hostname -I | awk '{print $1}')
 adb -s emulator-5554 shell am start -n ch.luethi.skylinestracker/ch.luethi.skylinestracker.MainActivity -a android.intent.action.MAIN -c android.intent.category.LAUNCHER -e ISTESTING true -e TESTING_IP ${ip}
-####python /home/andreas/IdeaProjects/SkyLinesTracker/Tests/integration.py
+####python $TEST_DIR/integration.py
 echo 'pos={11,22,530} motion={20,120,3} turnrate=10 tzdiff=0' | nc ktrax-sim.kisstech.ch 48888 | nc localhost 5554
