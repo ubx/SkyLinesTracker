79baffacb52e3f16807ff595e9f4b5b51c6eb45b
andreas <andreas.luethi@gmx.net>
2015-11-05 20:45:49 +0100

    Prepare for integration testing.

--- Tests/start-emulator.sh
+++ Tests/start-emulator.sh
@@ -3,18 +3,21 @@
 EMULATOR_DIR="/home/andreas/opt/android-sdk-linux/tools"
 PROJECT_DIR="/home/andreas/IdeaProjects/SkyLinesTracker"
 TEST_DIR="/home/andreas/IdeaProjects/SkyLinesTracker/Tests"
+IP=$(hostname -I | awk '{print $1}')
+
 #
 $EMULATOR_DIR/emulator -avd Device -netspeed full -netdelay none -no-boot-anim &
 adb -s emulator-5554 install -r $PROJECT_DIR/SkyLinesTracker.apk
-python preference_file.py 'ABC123' "5"  "false"  "true"  "192.168.1.43"
+
+python preference_file.py 'ABC123' "2"  "false"  "true"  $IP
 adb push $TEST_DIR/ch.luethi.skylinestracker_preferences.xml data/data/ch.luethi.skylinestracker/shared_prefs/
+
 adb -s emulator-5554 shell ps
 
 ###nc -l -u 5597 | xxd -b &
 
 adb shell input keyevent 82
-export ip=$(hostname -I | awk '{print $1}')
-adb -s emulator-5554 shell am start -n ch.luethi.skylinestracker/ch.luethi.skylinestracker.MainActivity -a android.intent.action.MAIN -c android.intent.category.LAUNCHER -e ISTESTING true -e TESTING_IP ${ip}
+adb -s emulator-5554 shell am start -n ch.luethi.skylinestracker/ch.luethi.skylinestracker.MainActivity -a android.intent.action.MAIN -c android.intent.category.LAUNCHER -e ISTESTING true -e TESTING_IP $IP
 java -jar $TEST_DIR/UDP-Receiver.jar &
 python $TEST_DIR/gps_simulator.py
 ###echo 'pos={11,22,530} motion={20,120,3} turnrate=10 tzdiff=0' | nc ktrax-sim.kisstech.ch 48888 | nc localhost 5554
