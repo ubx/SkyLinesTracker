edf4513e18bfd9e91d2a5c241f7dc21e3706cea0
andreas <andreas.luethi@gmx.net>
2016-08-13 11:13:32 +0200

    Add set variables.

--- Tests/scripts/run-battery-historian-server.sh
+++ Tests/scripts/run-battery-historian-server.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+export GOPATH=$HOME/work
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOBIN
 cd ~/work/src/github.com/google/battery-historian
 go run cmd/battery-historian/battery-historian.go
