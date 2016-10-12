9d7dc9ea4e2ca1f92675e25c101e58530cc14269
Andreas Lüthi <andreas.luethi@gmx.net>
2016-10-12 16:09:57 +0200

    Remove unnecessary onCreate().

--- src/ch/luethi/skylinestracker/SkyLinesApp.java
+++ src/ch/luethi/skylinestracker/SkyLinesApp.java
@@ -28,9 +28,4 @@ public class SkyLinesApp extends Application {
     public static FixQueueIF fixStack;
     public boolean doFixQueueing;
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-    }
-
 }
