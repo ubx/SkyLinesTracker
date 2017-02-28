89da218f0ca3c49022b05894a5b1a52c3a16cf4a
andreas <andreas.luethi@gmx.net>
2017-02-28 16:20:44 +0100

    Adjust test for switch button.

--- Tests/scripts/clickLiveTracking.sh
+++ Tests/scripts/clickLiveTracking.sh
@@ -4,25 +4,19 @@ TARGET=${2:-AVD}
 
 case $TARGET in
 AVD)
-    adb -s $1 shell sendevent /dev/input/event1 3  57   0
-    adb -s $1 shell sendevent /dev/input/event1 3  48   1
-    adb -s $1 shell sendevent /dev/input/event1 3  58   129
-    adb -s $1 shell sendevent /dev/input/event1 3  53   3693
-    adb -s $1 shell sendevent /dev/input/event1 3  54   8735
-    adb -s $1 shell sendevent /dev/input/event1 3   0   0
-    adb -s $1 shell sendevent /dev/input/event1 3  53   3762
-    adb -s $1 shell sendevent /dev/input/event1 3   0   0
-    adb -s $1 shell sendevent /dev/input/event1 3  53   3830
-    adb -s $1 shell sendevent /dev/input/event1 0   0   0
-    adb -s $1 shell sendevent /dev/input/event1 3  53   3899
-    adb -s $1 shell sendevent /dev/input/event1 0   0   0
-    adb -s $1 shell sendevent /dev/input/event1 3  57   4294967295
-    adb -s $1 shell sendevent /dev/input/event1 3  58   0
-    adb -s $1 shell sendevent /dev/input/event1 0   0   0
+    adb -s $1 shell sendevent /dev/input/event1 3 57   0
+    adb -s $1 shell sendevent /dev/input/event1 3 48   50
+    adb -s $1 shell sendevent /dev/input/event1 3 58   129
+    adb -s $1 shell sendevent /dev/input/event1 3 53   15186
+    adb -s $1 shell sendevent /dev/input/event1 3 54   6438
+    adb -s $1 shell sendevent /dev/input/event1 0 0    0
+    adb -s $1 shell sendevent /dev/input/event1 3 58   0
+    adb -s $1 shell sendevent /dev/input/event1 3 57   4294967295
+    adb -s $1 shell sendevent /dev/input/event1 0 0    0
     sleep 15
     ;;
 S3)
-    adb -s $1 shell input tap 116 244
+    adb -s $1 shell input tap 405 192
     sleep 5
     ;;
 
