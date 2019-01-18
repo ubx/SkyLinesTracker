434b0ea8717b89134e7368b8fd7f62ef556f460b
andreas <andreas.luethi@gmx.net>
2019-01-18 20:52:16 +0100

    Some minor refactoring.

--- app/src/main/java/ch/luethi/skylinestracker/SettingsActivity.java
+++ app/src/main/java/ch/luethi/skylinestracker/SettingsActivity.java
@@ -18,16 +18,10 @@
 
 package ch.luethi.skylinestracker;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.Configuration;
-import android.os.Build;
 import android.os.Bundle;
-import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
+import android.preference.*;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On handset devices, settings are presented
@@ -49,7 +43,6 @@ public class SettingsActivity extends PreferenceActivity {
 	@Override
 	protected void onPostCreate(Bundle savedInstanceState) {
 		super.onPostCreate(savedInstanceState);
-
 		setupSimplePreferencesScreen();
 	}
 
@@ -59,7 +52,7 @@ public class SettingsActivity extends PreferenceActivity {
 	 */
 	@SuppressWarnings("deprecation")
 	private void setupSimplePreferencesScreen() {
-		if (!isSimplePreferences(this)) {
+		if (isSimplePreferences(this)) {
 			return;
 		}
 
@@ -80,7 +73,7 @@ public class SettingsActivity extends PreferenceActivity {
 	/** {@inheritDoc} */
 	@Override
 	public boolean onIsMultiPane() {
-		return isXLargeTablet(this) && !isSimplePreferences(this);
+		return isXLargeTablet(this) && isSimplePreferences(this);
 	}
 
 	/**
@@ -91,39 +84,33 @@ public class SettingsActivity extends PreferenceActivity {
 	}
 
 	/**
-	 * Determines whether the simplified settings UI should be shown. This is true if this is forced via
-	 * {@link #ALWAYS_SIMPLE_PREFS}, or the device doesn't have newer APIs like {@link PreferenceFragment}, or the
-	 * device doesn't have an extra-large screen. In these cases, a single-pane "simplified" settings UI should be
-	 * shown.
+	 * Determines whether the simplified settings UI should be shown.
 	 */
 	private static boolean isSimplePreferences(Context context) {
-		return Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB || !isXLargeTablet(context);
+		return isXLargeTablet(context);
 	}
 
 	/**
 	 * A preference value change listener that updates the preference's summary to reflect its new value.
 	 */
-	private static final Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
-		@Override
-		public boolean onPreferenceChange(Preference preference, Object value) {
-			String stringValue = value.toString();
+	private static final Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = (preference, value) -> {
+		String stringValue = value.toString();
 
-			if (preference instanceof ListPreference) {
-				// For list preferences, look up the correct display value in
-				// the preference's 'entries' list.
-				ListPreference listPreference = (ListPreference) preference;
-				int index = listPreference.findIndexOfValue(stringValue);
+		if (preference instanceof ListPreference) {
+			// For list preferences, look up the correct display value in
+			// the preference's 'entries' list.
+			ListPreference listPreference = (ListPreference) preference;
+			int index = listPreference.findIndexOfValue(stringValue);
 
-				// Set the summary to reflect the new value.
-				preference.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);
+			// Set the summary to reflect the new value.
+			preference.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);
 
-			} else {
-				// For all other preferences, set the summary to the value's
-				// simple string representation.
-				preference.setSummary(stringValue);
-			}
-			return true;
+		} else {
+			// For all other preferences, set the summary to the value's
+			// simple string representation.
+			preference.setSummary(stringValue);
 		}
+		return true;
 	};
 
 	/**
